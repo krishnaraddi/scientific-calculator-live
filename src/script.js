d0362f4a0530169440c7b3316696a27447c12314
Krishnaraddi Kittur <krishnaraddi@gmail.com>
2024-01-11 14:15:53 +0530

    seperated out script file

---
+++ src/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code here
+    console.log('DOM is ready!');
+    const display =document.getElementById('calc-display');
+    const buttons=document.getElementsByClassName('btn');
+    console.log(buttons);
+    
+    let currentValue="";
+    
+    function evaluateResult(){
+        const convertedValue=currentValue
+        .replace("×", "*")
+        .replace("÷", "/")
+        .replace("%", "*0.01");
+        const result=eval(convertedValue);
+        currentValue=result.toString();
+        display.value=currentValue
+    }
+
+    for(let i=0; i<buttons.length; i++){
+        const button = buttons[i];
+        button.addEventListener('click', function(){
+            const value=button.innerText;
+
+            if(value=="AC"){
+                currentValue ="";
+                display.value=currentValue;
+            }else if(value == "="){
+                evaluateResult();
+
+            }else{
+                currentValue +=value;
+                display.value=currentValue;
+            }
+        });
+        
+    }
+
+   
+
+});
